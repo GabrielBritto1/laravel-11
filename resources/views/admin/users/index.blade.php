2caa57c59f60209548c21d45ebb1325b21393934
Gabriel Britto <gabriel_bianchibritto@hotmail.com>
2024-12-24 14:10:56 -0300

    Listando usuarios e paginação

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Usuarios</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>-</td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="3">Nenhum usuário no banco</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{$users->links()}}
+</body>
+
+</html>
