fe5b82633adaf424ec9811d4a4b147ddc6c5663d
Gabriel Britto <gabriel_bianchibritto@hotmail.com>
2025-01-11 10:21:13 -0300

    validacao avançada edicao

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -23,7 +24,7 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request)
     {
-        User::create($request->all());
+        User::create($request->validated());
         return redirect()->route('users.index')->with('success', 'Usuário criado com sucesso!');
     }
 
@@ -35,15 +36,17 @@ class UserController extends Controller
         return view('admin.users.edit', compact('user'));
     }
 
-    public function update(Request $request, string $id)
+    public function update(UpdateUserRequest $request, string $id)
     {
         if (!$user = User::find($id)) {
             return redirect()->back()->with('message', 'Usuário não encontrado!');
         }
-        $user->update($request->only([
-            'name',
-            'email'
-        ]));
+
+        $data = $request->only('name', 'email');
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
 
         return redirect()->route('users.index')->with('success', 'Usuário editado com sucesso!');
     }
